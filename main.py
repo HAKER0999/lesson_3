7c94f8f5954c867354cff52c3bcbfe338a4c024b
ALIJON0999 <ubaydullayevalijon4@gmail.com>
2023-11-29 20:59:31 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+
+#1-vazifa
+a = int(input("Juft sonni kiriting: "))
+if 2//a :
+    print("Tog'ri")
+
+else:
+      print("Notog'ri")
+
+ #2-vazifa
+age = int(input("yoshingizni kiriting: "))
+
+if (age < 4) + (age > 60):
+    print("bepul")
+
+elif age < 18 :
+     print("10000 sum")
+
+elif age > 18 :
+    print("20000 sum")
+
+else:
+       print(".")
+#2-vazifa tugadi
+
+#3-vazifa
+
+n = int(input("sonni kirit: "))
+print( f"Sizning javobingiz { n + 1} bo'ladi!")
+#3-vazifa tugadi
